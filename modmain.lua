89b8fbf68513acdef05f262994bc23ec9adfeb8f
Vasileios Vrouliotis <vvrouliotis@gmail.com>
2018-12-26 15:49:51 +0200

    Modified main.

--- modmain.lua
+++ modmain.lua
@@ -24,15 +24,15 @@ local require = GLOBAL.require
 
 -- The character select screen lines
 GLOBAL.STRINGS.CHARACTER_TITLES.abathur = "The Sample Character"
-GLOBAL.STRINGS.CHARACTER_NAMES.abathur = "Esc"
-GLOBAL.STRINGS.CHARACTER_DESCRIPTIONS.abathur = "*Perk 1\n*Perk 2\n*Perk 3"
-GLOBAL.STRINGS.CHARACTER_QUOTES.abathur = "\"Quote\""
+GLOBAL.STRINGS.CHARACTER_NAMES.abathur = "Abathur"
+GLOBAL.STRINGS.CHARACTER_DESCRIPTIONS.abathur = "*Spawn Toxic Nest\n*Mend\n*Biomass Generation"
+GLOBAL.STRINGS.CHARACTER_QUOTES.abathur = "\"Organism Abathur: with you.\""
 
 -- Custom speech strings
 GLOBAL.STRINGS.CHARACTERS.abathur = require "speech_abathur"
 
 -- Let the game know character is male, female, or robot
-table.insert(GLOBAL.CHARACTER_GENDERS.FEMALE, "abathur")
+table.insert(GLOBAL.CHARACTER_GENDERS.ROBOT, "abathur")
 
 
 AddMinimapAtlas("images/map_icons/abathur.xml")
